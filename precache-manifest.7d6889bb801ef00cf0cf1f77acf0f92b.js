41ecab12fded9ea8edd3a4204f9dbd7f03dfb404
Podmarkov Oleh <hicme@Podmarkovs-iMac.local>
2020-03-26 19:03:33 +0200

    upload

---
+++ precache-manifest.7d6889bb801ef00cf0cf1f77acf0f92b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635a60e4a19bc82fec9c3c201f794c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e876e94958ca7d62be7",
+    "url": "/static/css/main.b280f7cb.chunk.css"
+  },
+  {
+    "revision": "e0077de0728c648af2d0",
+    "url": "/static/js/2.307fd329.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.307fd329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e876e94958ca7d62be7",
+    "url": "/static/js/main.249a16d5.chunk.js"
+  },
+  {
+    "revision": "824a87c525fc41ffa25d",
+    "url": "/static/js/runtime-main.078ffda6.js"
+  }
+]);
